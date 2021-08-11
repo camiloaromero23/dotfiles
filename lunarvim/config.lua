6b9e12adb419a142fe3a5107c722a3660f5d1396
Camilo Romero <camiloaromero23@gmail.com>
2021-08-11 13:10:58 -0500

    Update nvim-tree preferences

--- lunarvim/config.lua
+++ lunarvim/config.lua
@@ -73,6 +73,8 @@ lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.hide_dotfiles = 0
+lvim.builtin.nvimtree.ignore = { ".git", ".cache" }
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = "maintained"
