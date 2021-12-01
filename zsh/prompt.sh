1ba6bd1d6a5ddb359f89a65eb26720490ee73d37
Camilo Romero <camiloaromero23@gmail.com>
2021-12-01 12:25:06 -0500

    Fix prompt on terminal resizing

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -4,6 +4,8 @@ setopt PROMPT_SUBST
 
 autoload colors && colors
 
+typeset +H return_code="%(?..%F{red}%?↵%f)"
+
 # Git configuration
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' enable git svn
@@ -12,17 +14,18 @@ git_prompt_info+=${parse_git_dirty}
 git_prompt_info+='%F{075})%f'
 zstyle ':vcs_info:git:*' formats ${git_prompt_info}
 
-precmd () { vcs_info }
+precmd () {
+  vcs_info
+
+  # Dashes
+  PS1='%F{237}'$(printf '-%.0s' {1..${COLUMNS}})'%f'
+  # Actual prompt
+  PS1+='%F{032}%~%f${vcs_info_msg_0_} %F{105}»%f '
+  # Right prompt
+  RPS1='${return_code}%F{237}%n@%m%f'
+}
 
 parse_git_dirty() {
   [[ -n $(git status --porcelain --untracked-files=no 2> /dev/null) ]] && echo '%F{214}*%f'
 }
 
-typeset +H return_code="%(?..%{$fg[red]%}%?↵%{$reset_color%})"
-
-# Dashes
-PS1='%F{237}'$(printf '-%.0s' {1..${COLUMNS}})'%f'
-# Actual prompt
-PS1+='%F{032}%~%f${vcs_info_msg_0_} %F{105}»%f '
-# Right prompt
-RPS1='${return_code}%F{237}%n@%m%f'
