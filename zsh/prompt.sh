002f5aaebe6d98ef822d7a8763335b95dfb0b799
Camilo Romero <camiloaromero23@gmail.com>
2021-12-01 13:08:21 -0500

    Fix partially terminal resizing issue

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -6,17 +6,8 @@ autoload colors && colors
 
 typeset +H return_code="%(?..%F{red}%?↵%f)"
 
-# Git configuration
-autoload -Uz vcs_info
-zstyle ':vcs_info:*' enable git svn
-git_prompt_info='%F{075}(%f%F{078}%b%f'
-git_prompt_info+=${parse_git_dirty}
-git_prompt_info+='%F{075})%f'
-zstyle ':vcs_info:git:*' formats ${git_prompt_info}
 
-precmd () {
-  vcs_info
-
+update_prompt () {
   # Dashes
   PS1='%F{237}'${(l:$COLUMNS::-:):-}'%f'
   # Actual prompt
@@ -25,7 +16,26 @@ precmd () {
   RPS1='${return_code}%F{237}%n@%m%f'
 }
 
+# Reload prompt on window resize
+TRAPWINCH () {
+  update_prompt
+}
+
+precmd () {
+  vcs_info
+
+  update_prompt
+}
+
 parse_git_dirty() {
   [[ -n $(git status --porcelain --untracked-files=no 2> /dev/null) ]] && echo '%F{214}*%f'
 }
+
+# Git configuration
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git svn
+git_prompt_info='%F{075}(%f%F{078}%b%f'
+git_prompt_info+=${parse_git_dirty}
+git_prompt_info+='%F{075})%f'
+zstyle ':vcs_info:git:*' formats ${git_prompt_info}
 
