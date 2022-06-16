d43a6e3c7f019f662a264de945f4c9c1cc5c1632
Camilo Romero <camilo.romero@kushkipagos.com>
2022-06-15 22:38:40 -0500

    Update notify command

--- zsh/plugins/notify.sh
+++ zsh/plugins/notify.sh
@@ -1,22 +1,6 @@
 # NOTE: Only works for kitty terminal
 
 notify() {
-  if [ "$#" -eq 1 ]; then
-    MESSAGE=$1
-    printf "\x1b]99;;$MESSAGE\x1b\\"
-    return 0
-  fi
-  printf "\x1b]99;i=1:d=1:p=body;$MESSAGE\x1b\\"
-  if [ "$#" -ne 2 ]; then
-    echo "Illegal number of parameters"
-    echo ""
-    echo "Usages:"
-    echo "\tnotify \$MESSAGE"
-    echo "\tnotify \$TITLE \$MESSAGE"
-    return 1
-  fi
-  TITLE=$1
-  MESSAGE=$2
-  printf "\x1b]99;i=1:d=0;$TITLE\x1b\\"
-  printf "\x1b]99;i=1:d=1:p=body;$MESSAGE\x1b\\"
+  MESSAGE="Process terminated"
+  printf "\x1b]99;;$MESSAGE\x1b\\"
 }
