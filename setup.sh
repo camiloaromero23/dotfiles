64f5375c21d3f4e7cbcdade2a41289bcf13fe688
Camilo Romero <camiloaromero23@gmail.com>
2023-06-05 12:51:36 -0500

    Include i3 config

--- setup.sh
+++ setup.sh
@@ -37,6 +37,10 @@ dotfiles+=( "" )
 locations+=( "git/.gitignore_global" )
 dotfiles+=( "" )
 
+# I3
+locations+=( "i3" )
+dotfiles+=( ".config/i3" )
+
 # Uncomment these if sens_conf needed
 # locations+=( "zsh/sens_conf" )
 # dotfiles+=( ".sens_conf" )
