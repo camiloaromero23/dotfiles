8c82c2c08c32ec080ba65a993618a9fe2f2e2a0d
Camilo Romero <camiloaromero23@gmail.com>
2023-10-06 09:03:22 -0500

    fix: Update command (exa no longer maintained)

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,7 +1,7 @@
 # Aliases
-alias la="exa -a -l -h --git --icons"
-alias lai="exa -a -l -h --git --icons --git-ignore"
-alias ls="exa --icons"
+alias la="eza -a -l -h --git --icons"
+alias lai="eza -a -l -h --git --icons --git-ignore"
+alias ls="eza --icons"
 alias cat="bat"
 alias lg="lazygit"
 alias sourcezsh="source $ZDOTDIR/.zshrc"
