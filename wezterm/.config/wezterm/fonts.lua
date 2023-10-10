337b37f4618132921f44c56d0b2834929bd1a3be
Camilo Romero <camiloaromero23@gmail.com>
2023-10-10 11:01:36 -0500

    refactor: Font config

--- wezterm/.config/wezterm/fonts.lua
+++ wezterm/.config/wezterm/fonts.lua
@@ -1,62 +1,64 @@
-local M = {}
 local wezterm = require "wezterm"
 local os = io.popen("uname"):read "l"
 
 local sf_mono_name = os == "Linux" and "SFMonoNerdFontMono" or "SFMono Nerd Font"
 
-M.sf_mono = wezterm.font {
-  family = sf_mono_name,
-  weight = "Medium",
-}
-M.sf_mono_rules = {
-  {
-    intensity = "Half",
-    italic = false,
-    font = wezterm.font {
-      family = sf_mono_name,
-      weight = "Regular",
-    },
-  },
-}
-
-M.sf_mono_size = 19.25
-
-M.caskaydia_cove = wezterm.font {
-  family = "CaskaydiaCove Nerd Font",
-  weight = "Medium",
-}
-
-M.caskaydia_cove_rules = {
-  {
-    intensity = "Half",
-    italic = false,
+local fonts = {
+  caskaydia_cove = {
     font = wezterm.font {
       family = "CaskaydiaCove Nerd Font",
-      weight = "Regular",
+      weight = "Medium",
     },
+    rules = {
+      {
+        intensity = "Half",
+        italic = false,
+        font = wezterm.font {
+          family = "CaskaydiaCove Nerd Font",
+          weight = "Regular",
+        },
+      },
+    },
+    size = os == "Linux" and 15.5 or 19.75,
+  },
+
+  sf_mono = {
+    font = wezterm.font {
+      family = sf_mono_name,
+      weight = "Medium",
+    },
+    rules = {
+      {
+        intensity = "Half",
+        italic = false,
+        font = wezterm.font {
+          family = sf_mono_name,
+          weight = "Regular",
+        },
+      },
+    },
+    size = 19.25,
+  },
+
+  dm_mono = {
+    font = wezterm.font {
+      family = "DM Mono",
+      weight = "Medium",
+    },
+    rules = {},
+    size = 19.75,
+    line_height = 1.15,
+  },
+
+  jetbrains_mono = {
+    font = wezterm.font {
+      family = "JetBrains Mono",
+      weight = "Medium",
+    },
+    rules = {},
+    size = 19.5,
+    line_height = 1.15,
   },
 }
 
-M.caskaydia_cove_size = os == "Linux" and 15.5 or 19.75
-
-M.jetbrains_mono_size = 19.5
-
-M.jetbrains_mono = wezterm.font {
-  family = "JetBrains Mono",
-  weight = "Medium",
-}
-
-M.jetbrains_mono_rules = {}
-M.jetbrains_mono_line_height = 1.15
-
-M.dm_mono_size = 19.75
-
-M.dm_mono = wezterm.font {
-  family = "DM Mono",
-  weight = "Medium",
-}
-
-M.dm_mono_rules = {}
-M.dm_mono_line_height = 1.15
-
-return M
+return fonts
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,16 +5,18 @@ local fonts = require "fonts"
 -- [caskaydia_cove, sf_mono, dm_mono, jetbrains_mono]
 local font_name = "caskaydia_cove"
 
+local font = fonts[font_name]
+
 -- === !== == != ++ -- => -> := :: www 0
 return {
   warn_about_missing_glyphs = false,
-  font = fonts[font_name],
-  font_rules = fonts[font_name .. "_rules"],
-  font_size = fonts[font_name .. "_size"],
+  font = font.font,
+  font_rules = font.rules,
+  font_size = font.size,
   -- color_scheme = "OneDark (base16)",
   colors = one_dark,
   audible_bell = "Disabled",
-  line_height = fonts[font_name .. "_line_height"] or 1.3,
+  line_height = font.line_height or 1.3,
   hide_tab_bar_if_only_one_tab = true,
   window_decorations = "RESIZE",
   hyperlink_rules = hyperlink_rules,
