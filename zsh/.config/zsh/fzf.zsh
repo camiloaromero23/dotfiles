84d73b06627a0d0e40134ad9968b5a6514cbf89c
Camilo Romero <camiloaromero23@gmail.com>
2023-10-31 12:46:16 -0500

    fix: Update fzf location for mac

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -1,6 +1,6 @@
 if [ -x "$(command -v fzf)" ]
 then
-  fzf_dir=$([ "$(uname)" = "Darwin" ] && echo "/opt/local/share/fzf/shell" || echo "/usr/share/fzf/")
+  fzf_dir=$([ "$(uname)" = "Darwin" ] && echo "/opt/homebrew/opt/fzf/shell" || echo "/usr/share/fzf/")
   source $fzf_dir/key-bindings.zsh
   source $fzf_dir/completion.zsh
 fi
