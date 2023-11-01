c0da691f2f110a203a7b7995780fe70dfd771014
Camilo Romero <camiloaromero23@gmail.com>
2023-11-01 12:09:07 -0500

    feat(wezterm-font): Include geist mono

--- wezterm/.config/wezterm/fonts.lua
+++ wezterm/.config/wezterm/fonts.lua
@@ -20,6 +20,16 @@ local fonts = {
     size = os == "Linux" and 15.5 or 19.75,
   },
 
+  geist_mono = {
+    font = wezterm.font {
+      family = "Geist Mono",
+      weight = "DemiBold",
+      harfbuzz_features = { "ss09=1" },
+    },
+    rules = {},
+    size = 19.5,
+    line_height = 1.225,
+  },
   jetbrains_mono = {
     font = wezterm.font {
       family = "JetBrainsMono Nerd Font",
