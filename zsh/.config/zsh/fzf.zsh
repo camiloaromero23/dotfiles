ad75c890f8eb3cb6f9d47e6bf7bb864c77ceaedd
Camilo Romero <camiloaromero23@gmail.com>
2023-12-23 21:36:23 -0500

    fix: Update fzf extensions location

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -1,6 +1,10 @@
 if [ -x "$(command -v fzf)" ]
 then
-  fzf_dir=$([ "$(uname)" = "Darwin" ] && echo "/opt/homebrew/opt/fzf/shell" || echo "/usr/share/fzf/")
+  fzf_dir=$([ "$(uname)" = "Darwin" ] && echo "/opt/homebrew/opt/fzf/shell" || echo "/usr/share/fzf")
+  if [ -d "/usr/share/doc/fzf/examples" ]
+  then
+    fzf_dir="/usr/share/doc/fzf/examples"
+  fi
   source $fzf_dir/key-bindings.zsh
   source $fzf_dir/completion.zsh
 fi
