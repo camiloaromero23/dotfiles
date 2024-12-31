e5fd290e1ed4e738a328c93cd0f03f28e661836f
Camilo Romero <camiloaromero23@gmail.com>
2024-12-31 13:53:08 -0500

    chore(wezterm): update config

--- wezterm/.config/wezterm/fonts.lua
+++ wezterm/.config/wezterm/fonts.lua
@@ -67,7 +67,7 @@ local fonts = {
     },
     rules = {},
     size = 19.5,
-    line_height = 1.2525,
+    line_height = 1.26,
   },
   jetbrains_mono = {
     font = wezterm.font {
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,7 @@
 local wezterm = require "wezterm"
+
+local config = wezterm.config_builder()
+
 local hyperlink_rules = require "hyperlinks"
 
 -- local one_dark = require "onehalf_dark"
@@ -9,38 +12,31 @@ local fonts = require "fonts"
 local font_name = "geist_mono"
 
 local font = fonts[font_name]
-
--- local colorschemes = {
---   one_dark = one_dark,
---   hybrid = hybrid,
--- }
--- local colorscheme_name = "hybrid"
--- local colorscheme = colorschemes[colorscheme_name]
-
 -- === !== == != ++ -- => -> := :: www 0
-return {
-  warn_about_missing_glyphs = false,
-  font = font.font,
-  font_rules = font.rules,
-  font_size = font.size,
-  color_scheme = "Catppuccin Mocha",
-  -- colors = colorscheme,
-  audible_bell = "Disabled",
-  line_height = font.line_height or 1.3,
-  hide_tab_bar_if_only_one_tab = true,
-  window_decorations = "RESIZE",
-  hyperlink_rules = hyperlink_rules,
-  window_padding = {
-    left = 4,
-    right = 0,
-    top = 7,
-    bottom = 0,
-  },
-  keys = {
-    {
-      key = "w",
-      mods = "CMD",
-      action = wezterm.action.CloseCurrentTab { confirm = true },
-    },
+
+local line_height = font.line_height
+
+config.warn_about_missing_glyphs = false
+config.font = font.font
+config.font_rules = font.rules
+config.font_size = font.size
+config.color_scheme = "Catppuccin Mocha"
+-- config.colors = colorscheme
+config.audible_bell = "Disabled"
+config.line_height = line_height or 1.3
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+config.hyperlink_rules = hyperlink_rules
+config.window_padding = {
+  left = 4,
+  right = 0,
+  top = 7,
+  bottom = 0,
+}
+config.keys = {
+  {
+    key = "w",
+    mods = "CMD",
+    action = wezterm.action.CloseCurrentTab { confirm = true },
   },
 }
